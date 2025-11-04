c9eddb9085b59e0ff245d222ebf0166e314b572a
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-11-04 06:39:11 -0500

    added time left in day information

--- output/output.go
+++ output/output.go
@@ -14,14 +14,8 @@ func OutputStats(w *tabwriter.Writer, work float64, start, current, goal record.
 	currentMinutes := current.TotalMinutes() + initialMinutes
 	athMinutes := goal.TotalMinutes()
 	goalPercentage := (currentMinutes / athMinutes) * 100
-	goalHr := goal.TotalMinutes() / 60.0
-	goalMin := math.Mod(goal.TotalMinutes(), 60.0)
-	fmt.Fprintf(w, "%s\t%.0fh %.0fm\t\n", goal.Name(), math.Floor(goalHr), goalMin)
 
-	curHr := math.Floor(currentMinutes / 60)
-	curMin := math.Mod(currentMinutes, 60.0)
 	fmt.Fprintf(w, "Today\t%.0fh %.0fm\t\n", math.Floor(current.TotalMinutes()/60), math.Mod(current.TotalMinutes(), 60.0))
-	fmt.Fprintf(w, "Work Done\t%.0fh %.0fm\t%.1f%%\t\n", math.Floor(curHr), curMin, goalPercentage)
 
 	if currentMinutes > athMinutes {
 		t := currentMinutes - athMinutes
@@ -31,7 +25,6 @@ func OutputStats(w *tabwriter.Writer, work float64, start, current, goal record.
 			goalPercentage-100)
 	} else {
 		//Calculate weekdays and weekend days
-		gapMin := athMinutes - initialMinutes
 		workDone := (currentMinutes - initialMinutes)
 
 		var minLeft float64
@@ -41,17 +34,16 @@ func OutputStats(w *tabwriter.Writer, work float64, start, current, goal record.
 				int(minLeft)/60, int(minLeft)%60, int(minLeft))
 			fmt.Printf("That's x<=%d pomodoros\n",
 				int(math.Ceil(minLeft/25.0)))
+			timeLeft -= (work - workDone)
+			if timeLeft > 0 {
+				fmt.Fprintf(w, "Time Left\t%.0fh %.0fm\t\n", math.Floor(timeLeft/60.0), math.Mod(timeLeft, 60.0))
+			}
 		} else if workDone == work {
 			fmt.Println("you've done all the work you needed to do today!")
 		} else {
 			minLeft = (workDone - work)
 			fmt.Printf("you've done %dhr %dm of extra work!\n",
 				int(minLeft)/60, int(minLeft)%60)
-		}
-		fmt.Fprintf(w, "Work Left\t%.0fh %.0fm\t%.1f%%\t\n", math.Floor((gapMin-workDone)/60.0), math.Mod((gapMin-workDone), 60.0), 100-goalPercentage)
-		timeLeft -= (work - workDone)
-		if timeLeft > 0 {
-			fmt.Fprintf(w, "Time Left\t%.0fh %.0fm\n", math.Floor(timeLeft/60.0), math.Mod(timeLeft, 60.0))
 		}
 	}
 	w.Flush()
